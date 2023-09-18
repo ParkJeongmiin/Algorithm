65f6b4a99fc706a96eca1ff7637d689aabeb99c0
ParkJeongmiin <88469147+ParkJeongmiin@users.noreply.github.com>
2023-09-18 20:12:14 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,7 +1,19 @@
 def solution(array, commands):
     answer = []
     
-    for i, j, k in commands:
-        answer.append(sorted(array[i-1: j])[k-1])
+    """
+    commands에서 ijk 받아오기
+    array[i+1, j+1] slicing
+    sort 해서 [k+1] 번째 숫자 answer.append()
+    """
+    
+    for command in commands:
+        i = command[0] - 1
+        j = command[1]
+        k = command[2] - 1
+        
+        sorted_array = array[i : j]
+        sorted_array.sort()
+        answer.append(sorted_array[k])
         
     return answer
