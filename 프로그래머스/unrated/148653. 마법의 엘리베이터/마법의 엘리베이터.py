06a9bec9487dd2e0e59bf3e5e684aadf07b41d68
ParkJeongmiin <88469147+ParkJeongmiin@users.noreply.github.com>
2023-10-04 20:11:42 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -0,0 +1,22 @@
+def solution(storey):
+    answer = 0
+    
+    while storey != 0:
+        
+        # 숫자 제일 뒤에 한자리 가져오기
+        check_num = storey % 10
+        storey = storey // 10
+        
+        # 돌 어떻게 쓸지 판단
+        if check_num < 5:       # 5 미만이면 -1 로 전부 쓴다.
+            answer += check_num
+        elif check_num == 5:
+            answer += 5
+            
+            if storey % 10 >= 5:
+                storey += 1
+        elif check_num > 5:
+            answer += 10 - check_num
+            storey += 1
+    
+    return answer
