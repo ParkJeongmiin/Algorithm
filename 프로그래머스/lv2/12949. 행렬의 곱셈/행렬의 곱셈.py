4554149c05989763f26b9a4688102f2fc16adf39
ParkJeongmiin <88469147+ParkJeongmiin@users.noreply.github.com>
2023-10-05 21:05:05 +0900

    [level 2] Title: 행렬의 곱셈, Time: 8.77 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -0,0 +1,14 @@
+def solution(arr1, arr2):
+    answer = [[0] * len(arr2[0]) for _ in range(len(arr1))]
+    
+    for i in range(len(arr1)):
+        for j in range(len(arr2[0])):
+            for k in range(len(arr1[0])):
+                answer[i][j] += arr1[i][k] * arr2[k][j]
+    
+    return answer
+'''
+행렬 곱셈
+
+m x n   n x b => m X b
+'''
