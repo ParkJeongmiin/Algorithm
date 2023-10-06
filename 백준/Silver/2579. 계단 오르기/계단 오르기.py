22b4ffcfa12b9b0a16d7450405c0eb60bd9aa31d
ParkJeongmiin <88469147+ParkJeongmiin@users.noreply.github.com>
2023-10-06 20:02:25 +0900

    [Silver III] Title: 계단 오르기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+stairs = [int(input()) for _ in range(n)]
+dp = [0] * n
+
+if len(stairs) <= 2:
+    print(sum(stairs))
+else:
+    dp[0] = stairs[0]
+    dp[1] = stairs[0] + stairs[1]
+    
+    for i in range(2, n):
+        dp[i] = max(dp[i-2] + stairs[i], dp[i-3] + stairs[i-1] + stairs[i])
+        
+    print(dp[-1])
+
