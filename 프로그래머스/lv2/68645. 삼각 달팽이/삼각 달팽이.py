b4fb86ebc7ad2c99972fc9e3bb5b0e51af91f1ba
ParkJeongmiin <88469147+ParkJeongmiin@users.noreply.github.com>
2023-10-07 19:07:51 +0900

    [level 2] Title: 삼각 달팽이, Time: 143.46 ms, Memory: 42.8 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -1,29 +1,26 @@
-def make_triangle(y, x, depth, cur_val, maps):
-    if depth <= 0:
-        return maps
-    if depth == 1:
-        maps[y][x] = cur_val
-    
-    for row in range(depth):
-        maps[y + row][x] = cur_val
-        cur_val += 1
-    for col in range(1, depth):
-        maps[y + depth -1][x + col] = cur_val
-        cur_val += 1
-    for idx in range(1, depth-1):
-        maps[y + depth - 1 - idx][x + depth - 1 - idx] = cur_val
-        cur_val += 1
-        
-    make_triangle(y + 2, x + 1, depth - 3, cur_val, maps)
-
 def solution(n):
-    answer = [[0] * n for n in range(1, n+1)]
-    a = []
+    answer = []
     
-    make_triangle(0, 0, n, 1, answer)
+    maps = [[0 for col in range(1, row + 1)] for row in range(1, n + 1)]     # 빈 삼각형 만들기
     
-    for row in answer:
+    y, x = -1, 0        # 제일 위에칸을 먼저 채워줘야해서 그 위의 칸에서 출발
+    value = 1
+    
+    for dir in range(n):
+        for _ in range(dir, n):
+            if dir % 3 == 0:
+                y += 1
+            elif dir % 3 == 1:
+                x += 1
+            else:
+                y -= 1
+                x -= 1
+                
+            maps[y][x] = value
+            value += 1
+    
+    for row in maps:
         for num in row:
-            a.append(num)
+            answer.append(num)
     
-    return a
+    return answer
