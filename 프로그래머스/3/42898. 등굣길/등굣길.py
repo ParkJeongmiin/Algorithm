8540474e46e969c476ec7c6b7fab33a902347a72
박정민 <88469147+ParkJeongmiin@users.noreply.github.com>
2025-04-28 16:42:18 +0900

    [level 3] Title: 등굣길, Time: 3.40 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -9,10 +9,10 @@ def solution(m, n, puddles):
     
     # 경우의 수 계산
     for i in range(1, n + 1):
-        for j in range(1, m + 1):            
+        for j in range(1, m + 1):                
             if dp[i][j] == -1:
                 dp[i][j] = 0
             else:
                 dp[i][j] += (dp[i - 1][j] + dp[i][j - 1]) % 1000000007
-        
-    return dp[n][m] % 1000000007
+    
+    return dp[n][m]
