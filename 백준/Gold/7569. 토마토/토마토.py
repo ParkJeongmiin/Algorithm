7a66adfb81dd864069f2046ba71f75e93c9dfa39
박정민 <88469147+ParkJeongmiin@users.noreply.github.com>
2026-03-30 15:39:23 +0900

    [Gold V] Title: 토마토, Time: 3624 ms, Memory: 58188 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -12,20 +12,23 @@ dy = [0, 0, -1, 1, 0, 0]
 dx = [0, 0, 0, 0, 1, -1]
 dh = [-1, 1, 0, 0, 0, 0]
 
-maps = [[[0] * M for _ in range(N)] for _ in range(H)]
+# maps 구성
+maps = []
+for h in range(H):
+    layer = []
 
-for i in range(H):
-    for j in range(N):
+    for y in range(N):
         row = list(map(int, input().split()))
 
-        for k in range(M):
-            maps[i][j][k] = row[k]
-
-            if row[k] == 1:  # 익은 토마토 좌표 저장, 방문 처리(중복 계산 방지)
-                cur_list.append((i, j, k))
-                maps[i][j][k] = 2
-            elif row[k] == 0:  # 익지 않은 토마토 개수 update
+        for x, val in enumerate(row):  # 인덱스 접근 오버헤드 최소화
+            if val == 1:
+                cur_list.append((h, y, x))
+            elif val == 0:
                 non_perfect += 1
+
+        layer.append(row)
+
+    maps.append(layer)
 
 # 시작부터 다 익은 경우 종료.
 if non_perfect == 0:
@@ -33,12 +36,12 @@ if non_perfect == 0:
     sys.exit()
 
 # 시뮬레이션
-next_list = []
 while cur_list:
     if non_perfect == 0:  # 모든 토마토가 익었을 경우 종료
         break
 
     day += 1
+    next_list = []
     for h, y, x in cur_list:  # 6 방향 확인
         for i in range(6):
             ny = y + dy[i]
@@ -47,12 +50,11 @@ while cur_list:
 
             if 0 <= ny < N and 0 <= nx < M and 0 <= nh < H:  # 범위 내 칸 조회 확인
                 if maps[nh][ny][nx] == 0:  # 익지 않은 토마토가 있는 칸인 경우
-                    maps[nh][ny][nx] = 2  # 방문 처리로 같은 날 중복 계산 방지
+                    maps[nh][ny][nx] = 1  # 방문 처리로 같은 날 중복 계산 방지
                     next_list.append((nh, ny, nx))
                     non_perfect -= 1
 
     cur_list = next_list
-    next_list = []
 
 if non_perfect == 0:
     print(day)
