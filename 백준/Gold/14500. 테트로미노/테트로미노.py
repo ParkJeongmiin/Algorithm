9666ee29f9e95787d2d89cbf007b8c5db041ba61
박정민 <88469147+ParkJeongmiin@users.noreply.github.com>
2026-03-31 14:34:53 +0900

    [Gold IV] Title: 테트로미노, Time: 4320 ms, Memory: 39928 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -0,0 +1,60 @@
+import sys
+
+# ----- input -----
+input = sys.stdin.readline
+N, M = map(int, input().split())
+
+# ----- code -----
+# initial settings
+answer = 0
+maps = []
+visited = [[False] * M for _ in range(N)]
+dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
+
+for _ in range(N):
+    layer = list(map(int, input().split()))
+    maps.append(layer)
+
+
+# dfs
+def dfs(y, x, depth, val):
+    global answer
+
+    if depth == 4:  # 기저 조건
+        answer = max(answer, val)
+        return
+
+    for i in range(4):  # 상하좌우 순회
+        ny = y + dy[i]
+        nx = x + dx[i]
+
+        if 0 <= ny < N and 0 <= nx < M and not visited[ny][nx]:
+            visited[ny][nx] = True  # y, x 방문 처리
+            dfs(ny, nx, depth + 1, val + maps[ny][nx])
+            visited[ny][nx] = False  # y, x 방문 처리 해제
+
+
+for y in range(N):
+    for x in range(M):
+        visited[y][x] = True
+        dfs(y, x, 1, maps[y][x])
+        visited[y][x] = False
+
+        # 'ㅏ' 모양 예외 처리
+        center = maps[y][x]
+
+        target_list = []
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+
+            if 0 <= ny < N and 0 <= nx < M:
+                target_list.append(maps[ny][nx])
+
+        if len(target_list) == 4:
+            answer = max(answer, center + sum(target_list) - min(target_list))
+        elif len(target_list) == 3:
+            answer = max(answer, center + sum(target_list))
+
+print(answer)
