96ac332588745fa73bb5d9b886f49a7692afbdb7
박정민 <88469147+ParkJeongmiin@users.noreply.github.com>
2026-04-08 20:09:26 +0900

    [level 1] Title: 노란불 신호등, Time: 244.46 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.py
@@ -0,0 +1,30 @@
+import math
+from functools import reduce
+
+
+def lcm(number_list):
+    return math.prod(number_list) // reduce(math.gcd, number_list)
+
+
+def check(t, signal):
+    G, Y, R = signal
+    return True if 1 <= (t % (G + Y + R)) - G <= Y else False
+    
+
+def solution(signals):
+    cycle_time = list(sum(s) for s in signals)
+    max_time = lcm(cycle_time)
+    t = 0
+    
+    while t <= max_time:
+        t += 1
+        
+        for s in signals:
+            if check(t, s):
+                continue
+            else:
+                break
+        else:
+            return t
+        
+    return -1
